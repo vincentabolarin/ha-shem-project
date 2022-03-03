c2a3b8a0142314001274f5c8706f4945c9c5b5d2
Vincent Abolarin <vincentabolarin@gmail.com>
2022-03-03 05:43:20 +0100

    add Calendar component to the right sidebar

--- src/components/RightSidebar.js
+++ src/components/RightSidebar.js
@@ -1,11 +1,13 @@
 import styles from '../styles/RightSidebar.module.css';
 
 import TryPro from './TryPro'
+import Calendar from './Calendar';
 
 const RightSidebar = () => {
     return (
         <div className={styles.content}>
             <TryPro />
+            <Calendar />
         </div>
     );
 }

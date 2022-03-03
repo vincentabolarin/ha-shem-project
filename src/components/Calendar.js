18cef299b6dad0d61d374d95b5b6762c524fdeac
Vincent Abolarin <vincentabolarin@gmail.com>
2022-03-03 10:42:09 +0100

    çhange icon for previous and next months to SVGs

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,24 +1,35 @@
 import styles from '../styles/Calendar.module.css';
 
+import previousArrow from '../assets/svg/previous-arrow.svg';
+import nextArrow from '../assets/svg/next-arrow.svg';
+
 const Calendar = () => {
     return (
         <div className={styles.content}>
             <div className={styles.months}>
                 <span className={styles.previousArrow}>
-                    &lt;
+                    <img src={previousArrow} alt="Previous Arrow" />
                 </span>
+                
                 <span className={styles.previousMonth}>
                     February
                 </span>
+
                 <span className={styles.currentMonth}>
                     March
                 </span>
+
                 <span className={styles.nextMonth}>
                     April
                 </span>
+
                 <span className={styles.nextArrow}>
-                    &gt;
+                <   img src={nextArrow} alt="Previous Arrow" />
                 </span>
+            </div>
+
+            <div className={styles.dayOfWeek}>
+                
             </div>
         </div>
     );

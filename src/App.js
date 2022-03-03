996b0e78e7aef630592054c5ceae950cac6a6ebe
Vincent Abolarin <vincentabolarin@gmail.com>
2022-03-03 01:38:59 +0100

    adjust display and positioning of components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import './App.css';
 
 import LeftSidebar from './components/LeftSidebar';
 import Topbar from './components/Topbar';
+import MainContent from './components/MainContent';
+import RightSidebar from './components/RightSidebar';
 
 function App() {
   return (
     <div className="App">
       <LeftSidebar />
-      <Topbar />
+      <div className="TopbarMainContent">
+        <Topbar />
+        <div className="RightSidebarMainContent">
+          <MainContent />
+          <RightSidebar />
+        </div>      
+      </div>  
     </div>
   );
 }

b096a0eacccd158c0ff6ad2b685a6d92fbbd83b0
Vincent Abolarin <vincentabolarin@gmail.com>
2022-03-03 04:01:29 +0100

    add legend to projects

--- src/components/ProjectProgress.js
+++ src/components/ProjectProgress.js
@@ -5,55 +5,80 @@ import userIcon from '../assets/jpg/vincent-abolarin.jpg';
 const ProjectProgress = () => {
     return (
         <div className={styles.content}>
-            <div className={styles.projectList}>
-                <div className={styles.project1}>
-                    <p className={styles.projectName}>
-                        App Design
-                    </p>
+            <div className={styles.projectsProgress}>
+                <div className={styles.projectList}>
+                    <div className={styles.project1}>
+                        <p className={styles.projectName}>
+                            App Design
+                        </p>
 
-                    <p className={styles.projectDate}>
-                        03 March, 2022
-                    </p>
+                        <p className={styles.projectDate}>
+                            03 March, 2022
+                        </p>
+                    </div>
+
+                    <div className={styles.project2}>
+                        <p className={styles.projectName}>
+                            App Design
+                        </p>
+
+                        <p className={styles.projectDate}>
+                            03 March, 2022
+                        </p>
+                    </div>
+
+                    <div className={styles.project3}>
+                        <p className={styles.projectName}>
+                            App Design
+                        </p>
+
+                        <p className={styles.projectDate}>
+                            03 March, 2022
+                        </p>
+                    </div>
                 </div>
 
-                <div className={styles.project2}>
-                    <p className={styles.projectName}>
-                        App Design
-                    </p>
+                <div className={styles.usersProgress}>
+                    <div className={styles.users}>
+                        <img src={userIcon} alt="User Icon" className={styles.userIcon} />
+                        <img src={userIcon} alt="User Icon" className={styles.userIcon} />
+                        <img src={userIcon} alt="User Icon" className={styles.userIcon} />
+                    </div>
 
-                    <p className={styles.projectDate}>
-                        03 March, 2022
-                    </p>
-                </div>
+                    <div className={styles.progress}>
+                        <div className={styles.progressBar}>
 
-                <div className={styles.project3}>
-                    <p className={styles.projectName}>
-                        App Design
-                    </p>
-
-                    <p className={styles.projectDate}>
-                        03 March, 2022
-                    </p>
+                        </div>
+                        <p className={styles.progressText}>
+                            80%
+                        </p>
+                    </div>
                 </div>
             </div>
+            <div className={styles.legend}>
+                <div className={`${styles.legendBar} ${styles.toDoBar}`}>
 
-            <div className={styles.usersProgress}>
-                <div className={styles.users}>
-                    <img src={userIcon} alt="User Icon" className={styles.userIcon} />
-                    <img src={userIcon} alt="User Icon" className={styles.userIcon} />
-                    <img src={userIcon} alt="User Icon" className={styles.userIcon} />
                 </div>
+                <p className={styles.legendText}>
+                    Todo
+                </p>
 
-                <div className={styles.progress}>
-                    <div className={styles.progressBar}>
+                <div className={`${styles.legendBar} ${styles.inProgressBar}`}>
 
-                    </div>
-                    <p className={styles.progressText}>
-                        80%
-                    </p>
                 </div>
+                <p className={styles.legendText}>
+                    In progress
+                </p>
+
+                <div className={`${styles.legendBar} ${styles.completedBar}`}>
+
+                </div>
+                <p className={styles.legendText}>
+                    Completed
+                </p>
             </div>
         </div>
+        
     );
 }
  

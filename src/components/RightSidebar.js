7dca4e483220fd119d48e360c9a233238533ded2
Vincent Abolarin <vincentabolarin@gmail.com>
2022-03-03 13:43:37 +0100

    bring in icons for use in MainContent

--- src/components/RightSidebar.js
+++ src/components/RightSidebar.js
@@ -2,12 +2,14 @@ import styles from '../styles/RightSidebar.module.css';
 
 import TryPro from './TryPro'
 import Calendar from './Calendar';
+import ClientMeeting from './ClientMeeting';
 
 const RightSidebar = () => {
     return (
         <div className={styles.content}>
             <TryPro />
             <Calendar />
+            <ClientMeeting />
         </div>
     );
 }
